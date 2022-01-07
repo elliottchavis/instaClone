c54feb0a293794715cafdb5fe6f1f861803c0d7a
johny babylon <ztredded@gmail.com>
2022-01-07 03:45:11 -0600

    added login

---
+++ instaClone/Controller/RegistrationController.swift
@@ -0,0 +1,21 @@
+//
+//  RegistrationController.swift
+//  instaClone
+//
+//  Created by elliott chavis on 1/7/22.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycles
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+}
