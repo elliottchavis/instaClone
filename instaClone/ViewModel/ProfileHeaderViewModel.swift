dc7ae58595c87246b626c5beb6cdb2f6f7afcfb9
johny babylon <ztredded@gmail.com>
2022-02-13 16:15:35 -0700

    added view model for profile header

---
+++ instaClone/ViewModel/ProfileHeaderViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileHeaderViewModel.swift
+//  instaClone
+//
+//  Created by elliott chavis on 2/13/22.
+//
+
+import Foundation
+
+struct ProfileHeaderViewModel {
+    let user: User
+    
+    var fullname: String {
+        return user.fullname
+    }
+    
+    var profileImageUrl: URL? {
+        return URL(string: user.profileImageUrl)
+    }
+    
+    init(user: User) {
+        self.user = user
+    }
+}
