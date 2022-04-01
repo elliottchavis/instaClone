9cb0675e781152d563c71146ca695ee25c0cb9c0
johny babylon <ztredded@gmail.com>
2022-04-01 07:58:56 -0600

    upload posts to firebase

--- instaClone/Controller/UploadPostController.swift
+++ instaClone/Controller/UploadPostController.swift
@@ -7,15 +7,24 @@
 
 import UIKit
 
+protocol UploadPostControllerDelegate: class {
+    func controllerDidFinishUploadingPost(_ controller: UploadPostController)
+}
+
 class UploadPostController: UIViewController {
     
     // MARK: - Properties
+    
+    weak var delegate: UploadPostControllerDelegate?
+    
+    var selectedImage: UIImage? {
+        didSet { photoImageView.image = selectedImage }
+    }
     
     private let photoImageView: UIImageView = {
         let iv = UIImageView()
         iv.contentMode = .scaleAspectFill
         iv.clipsToBounds = true
-        iv.image = #imageLiteral(resourceName: "venom-7")
         
         return iv
     }()
@@ -50,7 +59,17 @@ class UploadPostController: UIViewController {
     }
     
     @objc func didTapDone() {
-        print("DEBUG: Share post here...")
+        guard let image = selectedImage else { return }
+        guard let caption = captionTextView.text else { return }
+        
+        PostService.uploadPost(caption: caption, image: image) { error in
+            if let error = error {
+                print("DEBUG: Failed to upload post \(error.localizedDescription)")
+                return
+            }
+            
+            self.delegate?.controllerDidFinishUploadingPost(self)
+        }
     }
     
     // MARK: - Helpers
