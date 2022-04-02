95b648dfd533dec968aaefdfa9675cc96300b87d
johny babylon <ztredded@gmail.com>
2022-04-01 19:21:20 -0600

    fetch pics

---
+++ instaClone/ViewModel/PostViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  PostViewModel.swift
+//  instaClone
+//
+//  Created by elliott chavis on 4/1/22.
+//
+
+import Foundation
+
+struct PostViewModel {
+    private let post: Post
+    
+    var imageUrl: URL? {
+        return URL(string: post.imageUrl)
+    }
+    
+    var caption: String {
+        return post.caption
+    }
+    
+    var likes: Int {
+        return post.likes
+    }
+    
+    init(post: Post) {
+        self.post = post
+    }
+}
