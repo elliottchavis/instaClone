5bdcde28dc0add217497956bdaa770ad70fbad1b
johny babylon <ztredded@gmail.com>
2022-04-07 17:26:51 -0500

    added notification setup

---
+++ instaClone/View/NotificationCell.swift
@@ -0,0 +1,96 @@
+//
+//  NotificationCell.swift
+//  instaClone
+//
+//  Created by elliott chavis on 4/7/22.
+//
+
+import UIKit
+
+class NotificationCell: UITableViewCell {
+    
+    // MARK: - Properties
+    
+    private let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = #imageLiteral(resourceName: "venom-7")
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.backgroundColor = .lightGray
+        
+        return iv
+    }()
+    
+    private let infoLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.text = "venom"
+        return label
+    }()
+    
+    private let postImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.backgroundColor = .lightGray
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handlePostTapped))
+        iv.isUserInteractionEnabled = true
+        iv.addGestureRecognizer(tap)
+        
+        return iv
+    }()
+    
+    private let followButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Loading", for: .normal)
+        button.layer.cornerRadius = 3
+        button.layer.borderColor = UIColor.lightGray.cgColor
+        button.layer.borderWidth = 0.5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(self, action: #selector(handleFollowTapped), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        selectionStyle = .none
+        
+        addSubview(profileImageView)
+        profileImageView.setDimensions(height: 50, width: 50)
+        profileImageView.layer.cornerRadius = 50 / 2
+        profileImageView.centerY(inView: self, leftAnchor: leftAnchor, paddingLeft: 12)
+        
+        addSubview(infoLabel)
+        infoLabel.centerY(inView: profileImageView, leftAnchor: profileImageView.rightAnchor, paddingLeft: 8)
+        
+        addSubview(followButton)
+        followButton.centerY(inView: self)
+        followButton.anchor(right: rightAnchor, paddingRight: 12, width: 100, height: 32)
+        
+        addSubview(postImageView)
+        postImageView.centerY(inView: self)
+        postImageView.anchor(right: rightAnchor, paddingRight: 12, width: 40, height: 40)
+        
+        followButton.isHidden = true
+
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Actions
+    
+    @objc func handleFollowTapped() {
+        
+    }
+    
+    @objc func handlePostTapped() {
+        
+    }
+}
